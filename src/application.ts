106781352b844ac4a7e8eb04009d76e3a4ac9164
Kachitaro <anhtai741981@gmail.com>
2023-03-02 09:46:34 +0700

    done authorization

--- src/application.ts
+++ src/application.ts
@@ -9,15 +9,20 @@ import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
 import {MySequence} from './sequence';
-import { AuthenticationComponent } from "@loopback/authentication"
+import { AuthenticationComponent } from "@loopback/authentication";
 import {
   JWTAuthenticationComponent,
   TokenServiceBindings,
   UserServiceBindings,
-} from "@loopback/authentication-jwt"
-import { PostgresDataSource } from "./datasources"
+} from "@loopback/authentication-jwt";
+import {
+  AuthorizationComponent,
+  AuthorizationTags,
+} from '@loopback/authorization';
+import { PostgresDataSource } from "./datasources";
 import {MyUserService} from './services/users.service';
 import {myJWTService} from './services/jwt.service';
+import {MyAuthorizationProvider} from './services/provider.service';
 
 export {ApplicationConfig};
 
@@ -40,13 +45,21 @@ export class BeEnglishLbWithAuthenApplication extends BootMixin(
     this.component(RestExplorerComponent);
     // Mount authentication system
     this.component(AuthenticationComponent)
+    this.component(AuthorizationComponent);
+    //Bind customize provider
+    this.bind('authorizationProviders.my-authorizer-provider')
+      .toProvider(MyAuthorizationProvider)
+      .tag(AuthorizationTags.AUTHORIZER);
     // Mount jwt component
     this.component(JWTAuthenticationComponent)
     // Bind datasource
     this.dataSource(PostgresDataSource, UserServiceBindings.DATASOURCE_NAME)
     this.bind(UserServiceBindings.USER_SERVICE).toClass(MyUserService as any);
     this.bind(TokenServiceBindings.TOKEN_SERVICE).toClass(myJWTService as any);
-
+    // for jwt access token
+    this.bind(TokenServiceBindings.TOKEN_SECRET).to('my-jwt-secret-key');
+    // for jwt access token expiration
+    this.bind(TokenServiceBindings.TOKEN_EXPIRES_IN).to('86400')
 
     this.projectRoot = __dirname;
     // Customize @loopback/boot Booter Conventions here
--- src/services/authorizers.ts
+++ src/services/authorizers.ts
@@ -3,7 +3,6 @@ import {
   AuthorizationDecision,
   AuthorizationMetadata,
 } from '@loopback/authorization';
-import {securityId, UserProfile} from '@loopback/security';
 import _ from 'lodash';
 
 // Instance level authorizer
@@ -11,55 +10,27 @@ import _ from 'lodash';
 export async function basicAuthorization(
   authorizationCtx: AuthorizationContext,
   metadata: AuthorizationMetadata,
-): Promise<AuthorizationDecision> {
-  // No access if authorization details are missing
-  let currentUser: UserProfile;
+) {
+  const getId = await authorizationCtx.invocationContext.args[0];
+  console.log('ID: ', getId);
+
+  let currentUserId: number;
   if (authorizationCtx.principals.length > 0) {
     const user = _.pick(authorizationCtx.principals[0], [
-      'id',
+      'user_id',
       'name',
-      'roles',
+      'role',
     ]);
-    currentUser = {[securityId]: user.id, name: user.name, roles: user.roles};
+    console.log('USER: ', user);
+    currentUserId = user.user_id;
   } else {
     return AuthorizationDecision.DENY;
   }
+  console.log('IN META: ', metadata.allowedRoles);
+  console.log('userID', currentUserId, getId);
 
-  if (!currentUser.roles) {
-    return AuthorizationDecision.DENY;
-  }
-
-  // Authorize everything that does not have a allowedRoles property
-  if (!metadata.allowedRoles) {
-    return AuthorizationDecision.ALLOW;
-  }
-  let roleIsAllowed = false;
-  for (const role of currentUser.roles) {
-    if (metadata.allowedRoles!.includes(role)) {
-      roleIsAllowed = true;
-      break;
-    }
-  }
-
-  if (!roleIsAllowed) {
-    return AuthorizationDecision.DENY;
-  }
-
-  // Admin and support accounts bypass id verification
-  if (
-    currentUser.roles.includes('admin') ||
-    currentUser.roles.includes('support')
-  ) {
-    return AuthorizationDecision.ALLOW;
-  }
-
-  /**
-   * Allow access only to model owners, using route as source of truth
-   *
-   * eg. @post('/users/{userId}/orders', ...) returns `userId` as args[0]
-   */
-  if (currentUser[securityId] === authorizationCtx.invocationContext.args[0]) {
-    return AuthorizationDecision.ALLOW;
+  if (getId == currentUserId) {
+    return AuthorizationDecision.ABSTAIN;
   }
 
   return AuthorizationDecision.DENY;
